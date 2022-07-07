12375ade5b16a2ce765267f9a2c245af6e55eb2b
yurimarch <ypdmarch@gmail.com>
2022-07-07 23:55:56 +0200

    Removed unused classes

--- src/main/java/com/letscode/service/ProductService.java
+++ src/main/java/com/letscode/service/ProductService.java
@@ -2,7 +2,6 @@ package com.letscode.service;
 
 import com.letscode.dto.ProductRequest;
 import com.letscode.model.Product;
-import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
