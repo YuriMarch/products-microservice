7c3090d158e86a2111c654903b114997a2223b16
yurimarch <ypdmarch@gmail.com>
2022-07-13 14:45:49 +0200

    HealthCheckController added.

---
+++ src/main/java/com/letscode/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.letscode.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public String getHealthCheck() {
+        return "Products API is up and running.";
+    }
+}
